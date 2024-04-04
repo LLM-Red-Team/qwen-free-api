eab1767a4ec7f814148a7e0b8ff788775b14f12f
Vinlic <vinlic@vinlic.com>
2024-04-05 01:06:35 +0800

    补充欢迎页和错误请求引导，对小白更友好

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,8 +1,24 @@
+import fs from 'fs-extra';
+
+import Response from '@/lib/response/Response.ts';
 import chat from "./chat.ts";
 import images from "./images.ts";
 import ping from "./ping.ts";
 
 export default [
+    {
+        get: {
+            '/': async () => {
+                const content = await fs.readFile('public/welcome.html');
+                return new Response(content, {
+                    type: 'html',
+                    headers: {
+                        Expires: '-1'
+                    }
+                });
+            }
+        }
+    },
     chat,
     images,
     ping
